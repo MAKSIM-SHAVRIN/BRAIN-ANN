75b14becf67d931ab5dc06d558cd2ca484c80d3b
Maksim Shavrin <nutmegraw@gmail.com>
2023-11-27 19:07:43 +0500

    1) Unicode ranges for Ascii + cyrilic are added 2) Split sequenses by volumes function is added 3) get_element_by_fraction rewritten to get_index_by_fraction

--- utils.py
+++ utils.py
@@ -1,8 +1,22 @@
 from functools import wraps
-from random import choice
 from time import time
 
 from jaro import jaro_winkler_metric
+
+
+UNICODE_RANGES = [
+    (0x0020, 0x007E),  # ASCII punctuation and symbols, digits, latin
+    (0x0400, 0x045F),  # Cyrilic
+]
+
+
+def split_by_volumes(list_for_split: list, volumes: list):
+    resoult_lists = list()
+    for volume in volumes:
+        resoult_lists.append(list_for_split[:volume])
+        list_for_split = list_for_split[volume:]
+    resoult_lists.append(list_for_split)
+    return resoult_lists
 
 
 def get_unicode_characters_by_ranges(ranges: list[tuple]) -> str:
@@ -25,8 +39,8 @@ def dict_sum(dictionary_of_numbers: dict):
     return sum(dictionary_of_numbers.values())
 
 
-def get_element_by_fraction(sequence, fraction):
-    return sequence[round(fraction * (len(sequence) - 1))]
+def get_index_by_fraction(sequence, fraction):
+    return round(fraction * (len(sequence) - 1))
 
 
 def conv_int_to_list(number: int, width: int) -> list[int]:
@@ -40,10 +54,6 @@ def conv_int_to_list(number: int, width: int) -> list[int]:
 
 def conv_list_to_int(binaries_list: list) -> int:
     return sum([n*2**p for p, n in enumerate(reversed(binaries_list))])
-
-
-def generate_sign() -> int:
-    return choice((-1, 1,))
 
 
 def make_simple_structure(
@@ -79,3 +89,34 @@ def mix_in(*mixins):
 
 def split_by_evenodd_position(sequence) -> tuple:
     return (sequence[::2], sequence[1::2])
+
+
+def count_loss(self, loss_function, dataset):
+    losses, speeds, lengths = list(), list(), list()
+    for case in dataset:
+        time_limit = case['T']
+        request = case['Q'].replace('%T%', str(time_limit))
+        waited_response = case['A']
+
+        start = time()
+        real_response = self(request, time_limit=time_limit)
+        finish = time()
+        real_time = int(finish - start)
+
+        try:
+            print(f'RESPONSE {real_time}/{time_limit} sec: {real_response}')
+        except UnicodeEncodeError:
+            pass
+        responses_lengths = [len(waited_response), len(real_response)]
+
+        losses.append(loss_function(waited_response, real_response))
+        speeds.append(real_time / time_limit)
+        lengths.append(
+            max(responses_lengths) / (min(responses_lengths) + 1),
+        )
+    self.loss = mean(losses)
+    print(f'LOSS: {self.loss} ', end='')
+    self.speed = mean(speeds)
+    print(f'SPEED: {self.speed} ', end='')
+    self.length = mean(lengths)
+    print(f'LENGTH: {self.length} ')
