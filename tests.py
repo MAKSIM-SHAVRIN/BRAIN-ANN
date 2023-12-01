a2682a3783af99eb0d014236d4a5fd34b52cf2c8
Maksim Shavrin <nutmegraw@gmail.com>
2023-12-02 03:03:58 +0500

    neuron module test case is written

---
+++ tests.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, main
+
+from neuron import Neuron, count_arrays_product
+
+
+class NeuronTestCase(TestCase):
+    def test_count_arrays_product(self):
+        self.assertEqual(
+            first=count_arrays_product([-1, 1, -1], [-1, 1, 1]),
+            second=[1, 1, -1],
+        )
+
+    def test_count_arrays_product_wrong_lengths_arguments(self):
+        with self.assertRaises(ValueError):
+            count_arrays_product([1, -1, -1], [0, 0])
+
+    def test_Neuron__init__(self):
+        neuron = Neuron(inputs_number=4)
+        self.assertEqual(len(neuron.weights), 5)
+        neuron = Neuron(inputs_number=47)
+        self.assertEqual(len(neuron.weights), 48)
+
+    def test_Neuron__init__wrong_inputs_number(self):
+        with self.assertRaises(expected_exception=ValueError):
+            Neuron(inputs_number=1)
+
+
+if __name__ == '__main__':
+    main()
