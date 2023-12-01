5bccab3f8f74a5cb09af38645d89c18d625230a7
Maksim Shavrin <nutmegraw@gmail.com>
2023-12-02 03:01:08 +0500

    1) clause changed to strict one in stair function 2) same lengths of arguments check is added to count arrays production function 3) inputs number check is added to __init__() of Neuron 4) get_weighted sum is deleted cuz it`s extra 5) __call is rewritten with added comments

--- neuron.py
+++ neuron.py
@@ -2,25 +2,36 @@ from random import choice
 
 
 def heaviside(neuro_sum: float) -> int:
-    if neuro_sum >= 0:
-        return 1
-    return 0
+    if neuro_sum < 0:
+        return 0
+    return 1
 
 
-def count_arrays_product(first_array: list, second_array: list) -> list:
-    return [first * second for first, second in zip(first_array, second_array)]
+def count_arrays_product(array_1: list[int], array_2: list[int]) -> list[int]:
+    if len(array_1) != len(array_2):
+        raise ValueError('The arguments have various lengths')
+    return [num_1 * num_2 for num_1, num_2 in zip(array_1, array_2)]
 
 
 class Neuron:
     def __init__(self, inputs_number: int):
+        if inputs_number < 2:
+            raise ValueError('Neuron must have at least two non-bias input')
         # Add the 1 to inputs number for bias weight creating
         self.weights = [choice((-1, 1,)) for _ in range(inputs_number + 1)]
 
     def __call__(self, inputs_values: list[int]) -> int:
-        return heaviside(self._get_weighted_sum(inputs_values))
+        # Add bias is always 1
+        inputs_values.insert(index=0, object=1)
 
-    def _get_weighted_sum(self, inputs_values: list) -> int:
-        return sum(count_arrays_product(self.weights, inputs_values))
+        # Product inputs values on due weights
+        arrays_product = count_arrays_product(self.weights, inputs_values)
+
+        # Sum all values
+        neuron_sum = sum(arrays_product)
+
+        # Use activation function to get neuron output
+        return heaviside(neuron_sum)
 
     def __repr__(self):
         return f'\n< Neuron with weights: {self.weights} >'
