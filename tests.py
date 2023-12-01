5896ecb8540f89ff9a357e512b98d64bf2fc1610
Maksim Shavrin <nutmegraw@gmail.com>
2023-12-02 04:19:35 +0500

    testing of Neuron`s __call__() is added

--- tests.py
+++ tests.py
@@ -17,14 +17,30 @@ class NeuronTestCase(TestCase):
     def test_Neuron__init__(self):
         neuron = Neuron(inputs_number=4)
         self.assertEqual(len(neuron.weights), 5)
+
         neuron = Neuron(inputs_number=47)
         self.assertEqual(len(neuron.weights), 48)
 
     def test_Neuron__init__wrong_inputs_number(self):
         with self.assertRaises(expected_exception=ValueError):
             Neuron(inputs_number=1)
+
         with self.assertRaises(expected_exception=ValueError):
             Neuron(inputs_number=0)
+
+    def test_Neuron__call__(self):
+        neuron = object.__new__(Neuron)
+        neuron.weights = [-1, 1, -1, 1, -1]
+
+        # The case with weighted_sum > 0
+        self.assertEqual(neuron(inputs_values=[1, -1, 1, -1]), 1)
+
+        # The case with weighted_sum < 0
+        self.assertEqual(neuron(inputs_values=[-1, 1, -1, 1]), 0)
+
+        # The case with weighted_sum = 0
+        neuron.weights = [-1, 1, -1, 1]
+        self.assertEqual(neuron(inputs_values=[1, 1, 1]), 1)
 
 
 if __name__ == '__main__':
