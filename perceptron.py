8fec59da66ff666a28ca0d170813e07e77cae32d
Maksim Shavrin <nutmegraw@gmail.com>
2023-12-23 13:26:57 +0500

    module rewritten for numpy using numpy

--- perceptron.py
+++ perceptron.py
@@ -2,8 +2,9 @@ from itertools import chain
 from json import dump, load
 from pathlib import Path
 
+from numpy import array, int8, ndarray, concatenate
+
 from layer import Layer
-from neuron import Neuron  # Imported for annotation
 from utils import check_dir_path_slash_ending
 
 
@@ -15,43 +16,32 @@ class Perceptron:
         # but the next layer neuron's inputs number.
         self.layers = list()
         for layer_number, neurons_number in enumerate(structure[1:]):
-            self.layers.append(Layer(structure[layer_number], neurons_number))
+            self.layers.append(Layer(neurons_number, structure[layer_number]))
 
-    @classmethod
-    def init_with_weights(cls, structure: list[int], weights: list[int]):
-        perceptron = cls(structure)
-        if len(perceptron.all_weights) != len(weights):
-            raise ValueError('Wrong weights number')
-        for neuron in perceptron.all_neurons:
-            weights_number = len(neuron.weights)
-            neuron.weights = weights[:weights_number]
-            weights = weights[weights_number:]
-        return perceptron
+    def __call__(self, inputs_values: ndarray[int8]) -> ndarray[int8]:
+        resoults = inputs_values
+        for layer in self.layers:
+            resoults = layer(resoults)
+        return resoults
+
+    def __repr__(self) -> str:
+        return f'< Perceptron: {self.structure} >'
 
     @property
     def structure(self) -> list[int]:
         structure = list()
         for number, layer in enumerate(self.layers):
             if number == 0:
-                structure.append(len(layer.neurons[0].weights) - 1)
-            structure.append(len(layer.neurons))
+                structure.append(layer.matrix.shape[1] - 1)
+            structure.append(layer.matrix.shape[0])
         return structure
 
-    def __repr__(self) -> str:
-        return f'< Perceptron: {self.structure} >'
-
-    def __call__(self, inputs_values: list[int]) -> list[int]:
-        resoults = inputs_values
-        for layer in self.layers:
-            resoults = layer(resoults)
-        return resoults
-
-    def save(self, dir_path: str, file_name: str) -> str:
+    def save(self, dir_path, file_name) -> str:
         check_dir_path_slash_ending(dir_path)
         file = dir_path + file_name + '.perceptron'
         with open(file, mode='w', encoding='ascii') as filebuffer:
             dump(
-                obj=dict(structure=self.structure, weights=self.all_weights),
+                obj=[layer.matrix.tolist() for layer in self.layers],
                 fp=filebuffer,
             )
         return file
@@ -59,18 +49,24 @@ class Perceptron:
     @classmethod
     def load(cls, file: str):
         with open(file, mode='r', encoding='ascii') as filebuffer:
-            dictionary = load(fp=filebuffer)
-        structure = dictionary['structure']
-        weights = dictionary['weights']
-        return cls.init_with_weights(structure, weights)
+            loaded: list = load(fp=filebuffer)
+        perceptron = object.__new__(cls)
+        perceptron.layers = list()
+        for matrix in loaded:
+            layer = object.__new__(Layer)
+            layer.matrix = array(matrix, dtype=int8)
+            perceptron.layers.append(layer)
+        return perceptron
 
-    @property
-    def all_neurons(self) -> list[Neuron]:
-        return list(chain(*[layer.neurons for layer in self.layers]))
-
-    @property
-    def all_weights(self) -> list[int]:
-        return list(chain(*[neuron.weights for neuron in self.all_neurons]))
-
-    def __eq__(self, o):
+    def __eq__(self, o) -> bool:
         return self.layers == o.layers
+
+
+if __name__ == '__main__':
+    perceptron = Perceptron([7, 3, 7])
+    print(perceptron.save(str(Path(__file__).parent) + '/', 'save'))
+    perceptron_1 = Perceptron.load(
+        str(Path(__file__).parent) + '/save.perceptron',
+    )
+    print(perceptron == perceptron_1)
+    
